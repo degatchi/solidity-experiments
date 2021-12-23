6750d09e7b0bc1f1a420623ed780eaf061e96ac8
DeGatchi <72078332+DeGatchi@users.noreply.github.com>
2021-12-23 23:43:44 +1100

    Update LogicTests.sol

--- LogicTests.sol
+++ LogicTests.sol
@@ -12,7 +12,35 @@ contract Test {
 
     // when block.timestamp > futureTime, it will revert
     uint futureTime = block.timestamp + 5;
-    function timeRemaining() external view returns(uint) {
+    function check() external view returns(uint) {
         return futureTime - block.timestamp;
+    }
+
+    uint[] array;
+    function arrayChecker() external returns(uint length) {
+        array.push(1);
+        return array.length;
+    }
+
+    event GasUsed(string topic, uint gasUsed);
+
+    // -------------------
+    //  For Loop, + vs -
+    // -------------------
+
+    // 	25289 gas
+    function forUp() external {
+        uint gasStart = gasleft();
+        for (uint i; i < 10; i++) {}
+        uint gasUsed = gasStart = gasleft();
+        emit GasUsed('test1', gasUsed);
+    }
+
+    // 25209 gas
+    function forDown() external {
+        uint gasStart = gasleft();
+        for (uint i = 10; i > 0; i--) {}
+        uint gasUsed = gasStart = gasleft();
+        emit GasUsed('test1', gasUsed);
     }
 }
