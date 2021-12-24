d6d4d5c58ea9b2503cbf92e8da85e20a21a4786d
DeGatchi <72078332+DeGatchi@users.noreply.github.com>
2021-12-24 15:42:02 +1100

    Update LogicTests.sol

--- LogicTests.sol
+++ LogicTests.sol
@@ -82,4 +82,103 @@ contract Test {
         uint gasUsed = gasStart - gasleft();
         emit GasUsed(gasUsed);
     }
-}
+
+
+    // -----------------------------
+    //  != vs >
+    // -----------------------------
+
+    // 33 gas used
+    function largerThan() external {
+        uint number = 10;
+        uint gasStart = gasleft();
+        
+        require(9 < 10, "");
+
+        uint gasUsed = gasStart - gasleft();
+        emit GasUsed(gasUsed);
+    }
+
+    // 36 gas used
+    function notEqual() external {
+        uint number = 10;
+        uint gasStart = gasleft();
+        
+        require(9 != 10, "");
+
+        uint gasUsed = gasStart - gasleft();
+        emit GasUsed(gasUsed);
+    }
+
+
+    // -----------------------------
+    //  Array length
+    // -----------------------------
+
+    struct Array {
+        uint256[] array0;
+    }
+    
+    function checker() external pure returns(uint) {
+        Array memory structTest;
+
+        return structTest.array0.length;
+    }
+
+
+    // -----------------------------
+    //  Rounding
+    // -----------------------------
+
+    function round() external pure returns(uint daysPassed1, uint daysPassed2, uint daysPassed3) {
+        uint256 timePassed = 90000;
+        daysPassed1 = timePassed < 1 days ? 0 : timePassed / 86400;
+        daysPassed2 = timePassed / 86400;
+        daysPassed3 = timePassed / 100000;
+    }
+
+    
+    // -----------------------------
+    //  struct vs single line SSTORE
+    // -----------------------------
+
+    struct STRUCT {
+        uint256 a;
+        uint256 b;
+        address c;
+    }
+
+    mapping(uint8 => STRUCT) private structo;
+
+    // initialising: 46722 gas used
+    // after initialising: 6922 gas used
+    function structSSTORE() external {
+        uint gasStart = gasleft();
+
+        STRUCT memory s = STRUCT({
+            a: 1,
+            b: 1,
+            c: address(0)
+        });
+
+        structo[0] = s;
+
+        uint gasUsed = gasStart - gasleft();
+        emit GasUsed(gasUsed);
+    }
+
+    // initialising: 46618 gas used
+    // after initialising: 6818 gas used
+    function singleLineSSTORE() external {
+        uint gasStart = gasleft();
+
+        STRUCT storage s = structo[1];
+
+        s.a = 1;
+        s.b = 2;
+        s.c = address(0);
+
+        uint gasUsed = gasStart - gasleft();
+        emit GasUsed(gasUsed);
+    }
+}   
